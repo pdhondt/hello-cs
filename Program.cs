bbc8c7a8157835b98d8f272813790708c247b512
Peter <dhondt.peter@gmail.com>
2021-03-29 22:02:01 +0200

    created basic interactions

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace helloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello there!  What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi, " + name + ". What is your age?");
+            string age = Console.ReadLine();
+            Console.WriteLine(name + ", your age is " + age);
+
+
         }
     }
 }
